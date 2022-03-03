0224d763a12083d24a0185e40ec18438490a4646
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2022-03-02 19:56:33 -0800

    subscription ka model + controller done

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -4,3 +4,46 @@
 // 	DeletedAt
 
 // compound unique fkpackageid + fkcustomerid
+
+const { sequelize, DataTypes } = require('sequelize');
+const db = require('../../config/database');
+
+const Subscription = db.define(
+        "subscription",
+        {
+                FKCustomerID: {
+                        type: DataTypes.INTEGER,
+                        //  primaryKey: true,
+                        //  unique: true,
+                        //  autoIncrement: true
+                },
+                // first name
+                //last name
+                FKPackageID: {
+                        type: DataTypes.STRING(20),
+                        //  allowNull: false,
+                        //  unique: true,
+
+
+                },
+                CompoundPK: {
+                        type: DataTypes.STRING(20),
+                        allowNull: false,
+                        unique: true,
+                        primaryKey: true,
+                        // value: this.FKCustomerID.value + this.FKPackageID.value
+
+                },
+
+                deletedAt: {
+                        allowNull: false,
+                        type: sequelize.DATE
+                },
+
+        });
+
+Subscription.sync().then(() => {
+        console.log('table created');
+});
+
+module.exports = Subscription;
