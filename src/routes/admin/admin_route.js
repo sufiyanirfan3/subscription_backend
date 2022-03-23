ee43f4d4502685032023e643df6f23d410b5add4
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2022-03-23 12:10:37 -0700

    ok

--- src/routes/admin/admin_route.js
+++ src/routes/admin/admin_route.js
@@ -1,63 +1,62 @@
 const express = require('express');
 const router = express.Router();
 
-const adminController=require("../../controllers/admin/admin_controller")
-const userController=require("../../controllers/admin/user_controller")
-const packageController=require("../../controllers/admin/package_controller")
-const subscriptionController=require("../../controllers/admin/subscription_controller")
-const customerController=require("../../controllers/public/customer_controller")
+const adminController = require("../../controllers/admin/admin_controller")
+const userController = require("../../controllers/admin/user_controller")
+const packageController = require("../../controllers/admin/package_controller")
+const subscriptionController = require("../../controllers/admin/subscription_controller")
+const customerController = require("../../controllers/public/customer_controller")
 
+//checking
 router.post("/renewAccessToken", adminController.renewAccessToken);
 
-router.post('/adminSignIn',adminController.adminSignIn)
+router.post('/adminSignIn', adminController.adminSignIn)
 
-router.post('/addAdmin',adminController.authenticateAdmin,adminController.addAdmin)
+router.post('/addAdmin', adminController.authenticateAdmin, adminController.addAdmin)
 
-router.get('/getAdmins',adminController.getAdmins)
+router.get('/getAdmins', adminController.getAdmins)
 
-router.get('/getAdminById/:id',adminController.getAdminById)
+router.get('/getAdminById/:id', adminController.getAdminById)
 
-router.put('/updateProfile/:id',adminController.authenticateAdmin,adminController.updateProfile)
+router.put('/updateProfile/:id', adminController.authenticateAdmin, adminController.updateProfile)
 
-router.put('/deleteAdmin/:id',adminController.authenticateAdmin,adminController.deleteAdmin)
+router.put('/deleteAdmin/:id', adminController.authenticateAdmin, adminController.deleteAdmin)
 
-router.post('/changePassword',adminController.authenticateAdmin,adminController.changePassword)
+router.post('/changePassword', adminController.authenticateAdmin, adminController.changePassword)
 
-router.post('/logout',adminController.authenticateAdmin,adminController.logout)
+router.post('/logout', adminController.authenticateAdmin, adminController.logout)
 
-router.get('/getCustomerBySubscriptionId/:id',adminController.authenticateAdmin,adminController.getCustomerBySubscriptionId)
+router.get('/getCustomerBySubscriptionId/:id', adminController.authenticateAdmin, adminController.getCustomerBySubscriptionId)
 
 //user
-router.post('/addUser',adminController.authenticateAdmin,userController.addUser)
+router.post('/addUser', adminController.authenticateAdmin, userController.addUser)
 
-router.get('/getUsers',adminController.authenticateAdmin,userController.getUsers)
+router.get('/getUsers', adminController.authenticateAdmin, userController.getUsers)
 
-router.get('/getUserById/:id',adminController.authenticateAdmin,userController.getUserById)
+router.get('/getUserById/:id', adminController.authenticateAdmin, userController.getUserById)
 
-router.put('/suspendUser/:id',adminController.authenticateAdmin,userController.suspendUser)
+router.put('/suspendUser/:id', adminController.authenticateAdmin, userController.suspendUser)
 
-router.put('/temporarySuspendUser/:id',adminController.authenticateAdmin,userController.temporarySuspendUser)
+router.put('/temporarySuspendUser/:id', adminController.authenticateAdmin, userController.temporarySuspendUser)
 
-router.put('/deleteUser/:id',adminController.authenticateAdmin,userController.deleteUser)
+router.put('/deleteUser/:id', adminController.authenticateAdmin, userController.deleteUser)
 
 //package
-router.get('/getPackages',adminController.authenticateAdmin,packageController.getPackages)
+router.get('/getPackages', adminController.authenticateAdmin, packageController.getPackages)
 
-router.get('/getPackageById/:id',adminController.authenticateAdmin,packageController.getPackageById)
+router.get('/getPackageById/:id', adminController.authenticateAdmin, packageController.getPackageById)
 
-router.get('/getPackagesByUserId/:id',adminController.authenticateAdmin,packageController.getPackagesByUserId)
+router.get('/getPackagesByUserId/:id', adminController.authenticateAdmin, packageController.getPackagesByUserId)
 
 //subscription
-router.get('/getSubscriptions',adminController.authenticateAdmin,subscriptionController.getSubscriptions)
+router.get('/getSubscriptions', adminController.authenticateAdmin, subscriptionController.getSubscriptions)
 
-router.get('/getSubscriptionById/:id',adminController.authenticateAdmin,subscriptionController.getSubscriptionById)
+router.get('/getSubscriptionById/:id', adminController.authenticateAdmin, subscriptionController.getSubscriptionById)
 
 //customers
-router.get('/getCustomers',adminController.authenticateAdmin,customerController.getCustomers)
+router.get('/getCustomers', adminController.authenticateAdmin, customerController.getCustomers)
 
-router.get('/getCustomerById/:id',adminController.authenticateAdmin,customerController.getCustomerById)
-
-
+router.get('/getCustomerById/:id', adminController.authenticateAdmin, customerController.getCustomerById)
 
 
 
@@ -70,4 +69,6 @@ router.get('/getCustomerById/:id',adminController.authenticateAdmin,customerCont
 
 
 
-module.exports=router
+
+
+module.exports = router
